f1b95d203c12d8251b8268314bf55273e9832f7c
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-12 13:20:57 +0200

    Dyncfg module

---
+++ src/clj_dopey/dyncfg.clj
@@ -0,0 +1,23 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns clj-dopey.dyncfg)
+
+(def configuration
+    (atom nil))
+
+(def connection
+    (atom nil))
+
+(def bot-nick
+    (atom nil))
+
