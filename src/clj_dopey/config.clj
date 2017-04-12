7029ca1e23bc8ae8f22d7291cde21fb750130606
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-12 13:19:12 +0200

    Config.clj module

---
+++ src/clj_dopey/config.clj
@@ -0,0 +1,50 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns clj-dopey.config)
+
+(require '[clojure.pprint :as pprint])
+
+(require '[clojure-ini.core :as clojure-ini])
+
+(defn parse-int
+    "Parse the given string as an integer number."
+    [string]
+    (java.lang.Integer/parseInt string))
+
+(defn parse-float
+    "Parse the given string as a float number."
+    [string]
+    (java.lang.Float/parseFloat string))
+
+(defn parse-boolean
+    "Parse the given string as a boolean value."
+    [^String string]
+    (or (= string "true")
+        (= string "True")))
+
+(defn update-server-configuration
+    "Update server configuration from the loaded data."
+    [configuration]
+    (update-in configuration [:server :port] parse-int))
+
+(defn load-configuration
+    "Load configuration from the provided INI file."
+    [ini-file-name]
+    (-> (clojure-ini/read-ini ini-file-name :keywordize? true)
+        update-server-configuration))
+
+(defn print-configuration
+    "Print actual configuration to the output."
+    [configuration]
+    (pprint/pprint configuration))
+
