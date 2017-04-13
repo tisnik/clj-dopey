4816fee32da337ce59c285b235ea0c223c3a77fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-13 14:29:42 +0200

    Some core tests added

--- test/clj_dopey/core_test.clj
+++ test/clj_dopey/core_test.clj
@@ -1,7 +1,51 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns clj-dopey.core-test
   (:require [clojure.test :refer :all]
             [clj-dopey.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for various functions
+;
+
+(ns clj-dopey.core-test
+  (:require [clojure.test :refer :all]
+            [clj-dopey.core :refer :all]))
+
+
+(deftest test-start-bot-existence
+    "Check that the clj-dopey.core/start-bot definition exists."
+    (testing "if the clj-dopey.core/start-bot definition exists."
+        (is (callable? 'clj-dopey.core/start-bot))))
+
+
+(deftest test-show-help-existence
+    "Check that the clj-dopey.core/show-help definition exists."
+    (testing "if the clj-dopey.core/show-help definition exists."
+        (is (callable? 'clj-dopey.core/show-help))))
+
+
+(deftest test--main-existence
+    "Check that the clj-dopey.core/-main definition exists."
+    (testing "if the clj-dopey.core/-main definition exists."
+        (is (callable? 'clj-dopey.core/-main))))
+
