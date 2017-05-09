437c96c38fb57e3b81fa6651462e58141218f0cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-09 15:43:20 +0200

    Test for function create-reply

--- test/clj_dopey/irc_bot_test.clj
+++ test/clj_dopey/irc_bot_test.clj
@@ -129,3 +129,12 @@
         (is (not (message-for-me? "bot" "?" {:text "?" :target ""})))
         (is (not (message-for-me? "bot" "?" {:text "" :target ""})))))
 
+(deftest test-create-reply
+    "Check the behaviour of function clj-dopey.irc-bot/create-reply"
+    (testing "the function create-reply"
+        (are [x y] (= x (create-reply y))
+            {:target "#"}                  {:target "#"}
+            {:target "#channel"}           {:target "#channel"}
+            {:target "#channel message"}   {:target "#channel message"}
+            {:target "nick" :nick "nick"}  {:target "" :nick "nick"})))
+
