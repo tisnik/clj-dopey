64514fd342e290876108795824625877a6c61be0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:57:26 +0200

    Bump up Clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2017  Pavel Tisnovsky
+;  (C) Copyright 2017, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -15,7 +15,7 @@
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.7.0"]
+    :dependencies [[org.clojure/clojure "1.8.0"]
                    [irclj "0.5.0-alpha4"]
                    [clojure-ini "0.0.1"]
                    [org.clojure/java.jdbc "0.3.5"]
