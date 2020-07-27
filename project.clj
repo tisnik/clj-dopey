d131e4c0b28d16ea3fa3a4121af2da834b54982c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-27 12:40:44 +0200

    Reformatted project.clj by zprint

--- project.clj
+++ project.clj
@@ -11,28 +11,28 @@
 ;
 
 (defproject clj-dopey "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]
-                   [irclj "0.5.0-alpha4"]
-                   [clojure-ini "0.0.1"]
-                   [org.clojure/java.jdbc "0.3.5"]
-                   [org.xerial/sqlite-jdbc "3.7.2"]
-                   [org.clojure/data.csv  "0.1.2"]
-                   [org.clojure/tools.cli "0.3.1"]
-                   [org.clojure/tools.logging "0.3.1"]
-                   [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                      javax.jms/jms
-                                                      com.sun.jmdk/jmxtools
-                                                      com.sun.jmx/jmxri]]
-                   [org.slf4j/slf4j-log4j12 "1.6.6"]]
-    :main ^:skip-aot clj-dopey.core
-    :target-path "target/%s"
-    :plugins [[test2junit "1.1.0"]
-              [codox "0.8.11"]
-              [test2junit "1.1.0"]
-              [lein-cloverage "1.0.6"]]
-    :profiles {:uberjar {:aot :all}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [irclj "0.5.0-alpha4"]
+                 [clojure-ini "0.0.1"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/data.csv  "0.1.2"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]]
+  :main ^:skip-aot clj-dopey.core
+  :target-path "target/%s"
+  :plugins [[test2junit "1.1.0"]
+            [codox "0.8.11"]
+            [test2junit "1.1.0"]
+            [lein-cloverage "1.0.6"]]
+  :profiles {:uberjar {:aot :all}})
 
