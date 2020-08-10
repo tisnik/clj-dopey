8511f0bede163d1bc1bc5f51ab5d99f0ee6e5f47
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:02:37 +0200

    Reformatted db_interface.clj by zprint

--- src/clj_dopey/db_interface.clj
+++ src/clj_dopey/db_interface.clj
@@ -11,77 +11,83 @@
 ;
 
 (ns clj-dopey.db-interface
-    "Namespace that contains interface to the database.")
+  "Namespace that contains interface to the database.")
 
 (require '[clojure.java.jdbc  :as jdbc])
 
 (require '[clj-dopey.db-spec :as db-spec])
 
 (defn select-word-class-id
-    [input]
-    (-> (jdbc/query db-spec/dopey-db
-             ["select id from classes where class=?" input])
-        first
-        :id))
+  [input]
+  (-> (jdbc/query db-spec/dopey-db
+                  ["select id from classes where class=?" input])
+      first
+      :id))
 
 (defn select-source-id
-    [input]
-    (-> (jdbc/query db-spec/dopey-db
-             ["select id from sources where source=?" input])
-        first
-        :id))
+  [input]
+  (-> (jdbc/query db-spec/dopey-db
+                  ["select id from sources where source=?" input])
+      first
+      :id))
 
 (defn insert-word-into-dictionary
-    [term description word-class use-it incorrect-forms correct-forms
-     see-also internal verified copyrighted source]
-    (jdbc/insert! db-spec/dopey-db
-        :dictionary {:term            term
-                     :description     description
-                     :class           word-class
-                     :use             use-it
-                     :incorrect_forms incorrect-forms
-                     :correct_forms   correct-forms
-                     :see_also        see-also
-                     :internal        internal
-                     :verified        verified
-                     :copyrighted     copyrighted
-                     :source          source}))
+  [term description word-class use-it incorrect-forms correct-forms
+   see-also internal verified copyrighted source]
+  (jdbc/insert! db-spec/dopey-db
+                :dictionary {:term            term
+                             :description     description
+                             :class           word-class
+                             :use             use-it
+                             :incorrect_forms incorrect-forms
+                             :correct_forms   correct-forms
+                             :see_also        see-also
+                             :internal        internal
+                             :verified        verified
+                             :copyrighted     copyrighted
+                             :source          source}))
 
 (defn update-word
-    [word use-like]
-    (if use-like
-        (-> word .toLowerCase (.replaceAll "\\*" "%"))
-        (-> word .toLowerCase)))
+  [word use-like]
+  (if use-like
+    (-> word
+        .toLowerCase
+        (.replaceAll "\\*" "%"))
+    (-> word
+        .toLowerCase)))
 
 (defn add-where-clause
-    [word use-like]
-    (if use-like
-        "where lower(term) like ?"
-        "where lower(term)=?"))
+  [word use-like]
+  (if use-like
+      "where lower(term) like ?"
+      "where lower(term)=?"))
 
 (defn select-words
-    [word use-like]
-    (jdbc/query db-spec/dopey-db
-        [(str "select term, description,
+  [word use-like]
+  (jdbc/query
+    db-spec/dopey-db
+    [(str
+           "select term, description,
                  (select class from classes where classes.id=dictionary.class) as class,
                  use, incorrect_forms, correct_forms, see_also, internal, verified, copyrighted,
                  (select source from sources where sources.id=dictionary.source) as source,
                  (select product from products where products.id=dictionary.product) as product
-                 from dictionary " (add-where-clause word use-like)) (update-word word use-like)]))
+                 from dictionary "
+           (add-where-clause word use-like)) (update-word word use-like)]))
 
 (defn select-word-count
-    [word use-like]
-    (->
-        (jdbc/query db-spec/dopey-db
-            [(str "select count(*) as cnt from dictionary " (add-where-clause word use-like)) (update-word word use-like)])
-        first
-        :cnt))
+  [word use-like]
+  (->
+    (jdbc/query
+      db-spec/dopey-db
+      [(str "select count(*) as cnt from dictionary "
+                  (add-where-clause word use-like)) (update-word word use-like)])
+    first
+    :cnt))
 
 (defn all-words-count
-    []
-    (->
-        (jdbc/query db-spec/dopey-db
-            ["select count(*) as cnt from dictionary"])
-        first
-        :cnt))
+  []
+  (-> (jdbc/query db-spec/dopey-db ["select count(*) as cnt from dictionary"])
+      first
+      :cnt))
 
