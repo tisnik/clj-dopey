279c562768ece2615358461e392b46a091089fe4
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-19 14:40:39 +0200

    Reformatted dictionary.clj by zplot

--- src/clj_dopey/dictionary.clj
+++ src/clj_dopey/dictionary.clj
@@ -17,98 +17,98 @@
 ; best colors: 10 4 6
 ;
 (defn highlight
-    [string color-code]
-    (str (char 3) color-code string (char 3) "99"))
+  [string color-code]
+  (str (char 3) color-code string (char 3) "99"))
 
 (defn black
-    [string]
-    (highlight string "01"))
+  [string]
+  (highlight string "01"))
 
 (defn red
-    [string]
-    (highlight string "04"))
+  [string]
+  (highlight string "04"))
 
 (defn green
-    [string]
-    (highlight string "03"))
+  [string]
+  (highlight string "03"))
 
 (defn blue
-    [string]
-    (highlight string "02"))
+  [string]
+  (highlight string "02"))
 
 (defn purple
-    [string]
-    (highlight string "06"))
+  [string]
+  (highlight string "06"))
 
 (defn yellow
-    [string]
-    (highlight string "08"))
+  [string]
+  (highlight string "08"))
 
 (defn bold
-    [string]
-    (str (char 2) string (char 15)))
+  [string]
+  (str (char 2) string (char 15)))
 
 (def reset
-    (str (char 15)))
+  (str (char 15)))
 
 (defn yes-no-with-caution
-    [w key]
-    (condp = (get w key)
-          0 (red    "no")
-          1 (green  "yes")
-          2 (yellow "with caution")
-            (red    "error - word usage is not set!")))
+  [w key]
+  (condp = (get w key)
+    0 (red    "no")
+    1 (green  "yes")
+    2 (yellow "with caution")
+      (red    "error - word usage is not set!")))
 
 (defn print-field
-    [title word key]
-    (str (bold (str title ": ")) (get word key) " "))
+  [title word key]
+  (str (bold (str title ": ")) (get word key) " "))
 
 (defn use-it
-    [w]
-    (str (bold "use it: ") (yes-no-with-caution w :use) " "))
+  [w]
+  (str (bold "use it: ") (yes-no-with-caution w :use) " "))
 
 (defn incorrect-forms
-    [w]
-    (str reset
-         (print-field "class"           w :class)
-         (use-it                        w)
-         (print-field "description"     w :description)
-         (print-field "source"          w :source)
-         (print-field "incorrect forms" w :incorrect_forms)
-         (print-field "see also"        w :see_also)
-         (print-field "product"         w :product)))
+  [w]
+  (str reset
+       (print-field "class"           w :class)
+       (use-it                        w)
+       (print-field "description"     w :description)
+       (print-field "source"          w :source)
+       (print-field "incorrect forms" w :incorrect_forms)
+       (print-field "see also"        w :see_also)
+       (print-field "product"         w :product)))
 
 (defn correct-forms
-    [w]
-    (str reset
-         (print-field "class"           w :class)
-         (use-it                        w)
-         (print-field "description"     w :description)
-         (print-field "source"          w :source)
-         (print-field "correct forms"   w :correct_forms)
-         (print-field "see also"        w :see_also)
-         (print-field "product"         w :product)))
+  [w]
+  (str reset
+       (print-field "class"           w :class)
+       (use-it                        w)
+       (print-field "description"     w :description)
+       (print-field "source"          w :source)
+       (print-field "correct forms"   w :correct_forms)
+       (print-field "see also"        w :see_also)
+       (print-field "product"         w :product)))
 
 (defn preferred-forms
-    [w]
-    (str reset
-         (print-field "class"           w :class)
-         (use-it                        w)
-         (print-field "description"     w :description)
-         (print-field "source"          w :source)
-         (print-field "preferred forms" w :correct_forms)
-         (print-field "see also"        w :see_also)
-         (print-field "product"         w :product)))
+  [w]
+  (str reset
+       (print-field "class"           w :class)
+       (use-it                        w)
+       (print-field "description"     w :description)
+       (print-field "source"          w :source)
+       (print-field "preferred forms" w :correct_forms)
+       (print-field "see also"        w :see_also)
+       (print-field "product"         w :product)))
 
 (defn unknown-forms
-    [w]
-    (str reset
-         (print-field "class"           w :class)
-         (use-it                        w)
-         (print-field "description"     w :description)
-         (print-field "source"          w :source)
-         (print-field "see also"        w :see_also)
-         (print-field "product"         w :product)))
+  [w]
+  (str reset
+       (print-field "class"           w :class)
+       (use-it                        w)
+       (print-field "description"     w :description)
+       (print-field "source"          w :source)
+       (print-field "see also"        w :see_also)
+       (print-field "product"         w :product)))
 
 (defn find-word
     [word]
