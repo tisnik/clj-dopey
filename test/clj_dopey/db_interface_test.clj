12d69261e733fc880b391f6687b7ce0a3e905c58
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-25 09:32:12 +0200

    Reformatted db_interface_test.clj by zprint

--- test/clj_dopey/db_interface_test.clj
+++ test/clj_dopey/db_interface_test.clj
@@ -19,46 +19,48 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions
 ;
 
 (deftest test-select-word-class-id-existence
-    "Check that the clj-dopey.db-interface/select-word-class-id definition exists."
-    (testing "if the clj-dopey.db-interface/select-word-class-id definition exists."
-        (is (callable? 'clj-dopey.db-interface/select-word-class-id))))
+  "Check that the clj-dopey.db-interface/select-word-class-id definition exists."
+  (testing
+    "if the clj-dopey.db-interface/select-word-class-id definition exists."
+    (is (callable? 'clj-dopey.db-interface/select-word-class-id))))
 
 
 (deftest test-select-source-id-existence
-    "Check that the clj-dopey.db-interface/select-source-id definition exists."
-    (testing "if the clj-dopey.db-interface/select-source-id definition exists."
-        (is (callable? 'clj-dopey.db-interface/select-source-id))))
+  "Check that the clj-dopey.db-interface/select-source-id definition exists."
+  (testing "if the clj-dopey.db-interface/select-source-id definition exists."
+           (is (callable? 'clj-dopey.db-interface/select-source-id))))
 
 
 (deftest test-insert-word-into-dictionary-existence
-    "Check that the clj-dopey.db-interface/insert-word-into-dictionary definition exists."
-    (testing "if the clj-dopey.db-interface/insert-word-into-dictionary definition exists."
-        (is (callable? 'clj-dopey.db-interface/insert-word-into-dictionary))))
+  "Check that the clj-dopey.db-interface/insert-word-into-dictionary definition exists."
+  (testing
+    "if the clj-dopey.db-interface/insert-word-into-dictionary definition exists."
+    (is (callable? 'clj-dopey.db-interface/insert-word-into-dictionary))))
 
 
 (deftest test-select-words-existence
-    "Check that the clj-dopey.db-interface/select-words definition exists."
-    (testing "if the clj-dopey.db-interface/select-words definition exists."
-        (is (callable? 'clj-dopey.db-interface/select-words))))
+  "Check that the clj-dopey.db-interface/select-words definition exists."
+  (testing "if the clj-dopey.db-interface/select-words definition exists."
+           (is (callable? 'clj-dopey.db-interface/select-words))))
 
 
 (deftest test-select-word-count-existence
-    "Check that the clj-dopey.db-interface/select-word-count definition exists."
-    (testing "if the clj-dopey.db-interface/select-word-count definition exists."
-        (is (callable? 'clj-dopey.db-interface/select-word-count))))
+  "Check that the clj-dopey.db-interface/select-word-count definition exists."
+  (testing "if the clj-dopey.db-interface/select-word-count definition exists."
+           (is (callable? 'clj-dopey.db-interface/select-word-count))))
 
 
 (deftest test-all-words-count-existence
-    "Check that the clj-dopey.db-interface/all-words-count definition exists."
-    (testing "if the clj-dopey.db-interface/all-words-count definition exists."
-        (is (callable? 'clj-dopey.db-interface/all-words-count))))
+  "Check that the clj-dopey.db-interface/all-words-count definition exists."
+  (testing "if the clj-dopey.db-interface/all-words-count definition exists."
+           (is (callable? 'clj-dopey.db-interface/all-words-count))))
 
