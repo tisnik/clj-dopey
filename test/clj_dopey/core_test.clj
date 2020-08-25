5b2997304c36b495f5914f931f797fbb9f3c2733
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-25 09:32:04 +0200

    Reformatted core_test.clj by zprint

--- test/clj_dopey/core_test.clj
+++ test/clj_dopey/core_test.clj
@@ -20,9 +20,9 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions
@@ -34,42 +34,42 @@
 
 
 (deftest test-start-bot-existence
-    "Check that the clj-dopey.core/start-bot definition exists."
-    (testing "if the clj-dopey.core/start-bot definition exists."
-        (is (callable? 'clj-dopey.core/start-bot))))
+  "Check that the clj-dopey.core/start-bot definition exists."
+  (testing "if the clj-dopey.core/start-bot definition exists."
+           (is (callable? 'clj-dopey.core/start-bot))))
 
 
 (deftest test-show-help-existence
-    "Check that the clj-dopey.core/show-help definition exists."
-    (testing "if the clj-dopey.core/show-help definition exists."
-        (is (callable? 'clj-dopey.core/show-help))))
+  "Check that the clj-dopey.core/show-help definition exists."
+  (testing "if the clj-dopey.core/show-help definition exists."
+           (is (callable? 'clj-dopey.core/show-help))))
 
 
 (deftest test-run-app-existence
-    "Check that the clj-dopey.core/run-app definition exists."
-    (testing "if the clj-dopey.core/run-app definition exists."
-        (is (callable? 'clj-dopey.core/run-app))))
+  "Check that the clj-dopey.core/run-app definition exists."
+  (testing "if the clj-dopey.core/run-app definition exists."
+           (is (callable? 'clj-dopey.core/run-app))))
 
 (deftest test--main-existence
-    "Check that the clj-dopey.core/-main definition exists."
-    (testing "if the clj-dopey.core/-main definition exists."
-        (is (callable? 'clj-dopey.core/-main))))
+  "Check that the clj-dopey.core/-main definition exists."
+  (testing "if the clj-dopey.core/-main definition exists."
+           (is (callable? 'clj-dopey.core/-main))))
 
 (deftest test-show-help
-    "Check the function show-help"
-    (testing "the function show-help"
-       ; use mock instead of println function
-        (with-redefs [println (fn [string] string)]
-            (is (= "help"         (show-help "help")))
-            (is (= "line1\nline2" (show-help "line1\nline2"))))))
+  "Check the function show-help"
+  (testing "the function show-help"
+           ; use mock instead of println function
+           (with-redefs [println (fn [string] string)]
+             (is (= "help"         (show-help "help")))
+             (is (= "line1\nline2" (show-help "line1\nline2"))))))
 
 (deftest test-run-app
-    "Check the function run-app"
-    (testing "the function run-app"
-        (with-redefs [show-help   (fn [summary] :help)
-                      import-data (fn [import?] :import)
-                      start-bot   (fn [] :start-bot)]
-            (is (= :help      (run-app "summary" true false)))
-            (is (= :import    (run-app "summary" false true)))
-            (is (= :start-bot (run-app "summary" false false))))))
+  "Check the function run-app"
+  (testing "the function run-app"
+           (with-redefs [show-help   (fn [summary] :help)
+                         import-data (fn [import?] :import)
+                         start-bot   (fn [] :start-bot)]
+             (is (= :help      (run-app "summary" true false)))
+             (is (= :import    (run-app "summary" false true)))
+             (is (= :start-bot (run-app "summary" false false))))))
 
